a7836697e8f29dd593408f48869c9ae2a9a47f16
Fayza Mohsen <fayzamohsen11@gmail.com>
2026-08-01 22:59:24 +0300

    Add files via upload

---
+++ driver/driver.v
@@ -0,0 +1,13 @@
+module driver#(
+  parameter WIDTH=8
+)
+(
+  input wire [WIDTH-1:0] data_in,
+  input wire data_en,
+  
+  output wire [WIDTH-1:0] data_out
+);
+
+  assign data_out= (data_en) ? data_in : { WIDTH{1'bZ} } ;
+
+endmodule
